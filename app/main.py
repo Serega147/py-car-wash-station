541a333d97c29db410f2f0ceb810f0118e1aca50
Serhii Khristov <serega.khristov17@gmail.com>
2024-10-14 23:10:48 +0300

    fix3

--- app/main.py
+++ app/main.py
@@ -17,8 +17,8 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
             price = (car.comfort_class * (
-                self.clean_power - car.clean_mark) *
-                     self.average_rating / self.distance_from_city_center)
+                self.clean_power - car.clean_mark)
+                     * self.average_rating / self.distance_from_city_center)
             return round(price, 1)
         return 0
 
